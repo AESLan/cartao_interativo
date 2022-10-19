bfc17b0e83334dbf1e7be9ce0dc0ea354673acce
unknown <alexmusicalsa@gmail.com>
2022-10-19 20:44:00 -0300

    Novo readme e alterações nos forms

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# cartao_interativo
+## Projeto de um cartão de crédito interativo: 
+## O usuário entra com os dados fictícios de um cartão de crédito.
+## Apenas para visualização do cartão pronto.
--- script/script.js
+++
@@ -1,8 +0,0 @@
-function getValues() {
-  let nome = $("#username").val();
-  let num = $("#number").val();
-  let valid = $("#validade").val();
-  let cvc = $("#cvc").val();
-
-  $(".cartaofrente").html(nome + " - " + valid + cvc);
-}
